9c14d2736bca973888ae4dbf6c7aa85b9d082058
kpbongat <kpbongat.dev@gmail.com>
2024-08-08 21:15:53 +0800

    Create Node and Tree classes

---
+++ index.js
@@ -0,0 +1,13 @@
+class Node {
+  constructor(data, left, right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Tree {
+  constructor(root) {
+    this.root = root;
+  }
+}
